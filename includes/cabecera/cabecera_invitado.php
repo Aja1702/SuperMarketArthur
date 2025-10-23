6cd0a33a4f6d1ec33e813ee8f46a71147b30d24f
Arturo <arturojimenezaparicio@gmail.com>
2025-10-23 12:25:00 +0200

    actualizacion header

--- includes/cabecera/cabecera_invitado.php
+++ includes/cabecera/cabecera_invitado.php
@@ -1,12 +1,15 @@
 <header class="cabecera-invitado">
     <div class="logo">
-        <img src="./img/logo/logo_supermarket.png" alt="Logo SupermarketArthur">
+        <a href="./" aria-label="Ir a la página principal">
+            <img src="./img/logo/logo_supermarket.png" alt="Logo SupermarketArthur" />
+        </a>
     </div>
-    <h1 class="titulo-web-invitado">
-        SuperMarketArthur
-    </h1>
+    <h1 class="titulo-web-invitado">SuperMarketArthur</h1>
     <nav class="acceso-usuario" aria-label="Acceso de usuario">
         <a href="./?userSession=login" class="btn-inicio-sesion">Iniciar sesión</a>
         <a href="./?userSession=registro" class="btn-registro">Registrarse</a>
+        <a href="./?vistaMenu=carrito" class="btn-carrito" aria-label="Ver carrito de compras">
+            🛒 Carrito
+        </a>
     </nav>
 </header>
