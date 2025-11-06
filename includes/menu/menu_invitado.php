07702f21997752c2ead050b2e356618b2dd8ecc1
Arturo <arturojimenezaparicio@gmail.com>
2025-11-06 09:58:21 +0100

    actualización necesaria now

--- includes/menu/menu_invitado.php
+++ includes/menu/menu_invitado.php
@@ -1,42 +1,28 @@
 <nav class="menu-horizontal">
-    <input type="checkbox" id="menuToggle" class="menu-toggle" />
-    <label for="menuToggle" class="menu-icon">&#9776;</label> <!-- icono hamburguesa -->
     <ul>
         <li>
             <a href="../../SuperMarketArthur/">
                 <img src="./img/menu_nav/home.png" alt="Inicio">
-                <p>Inicio</p>
+                <span>Inicio</span>
             </a>
         </li>
         <li>
             <a href="./?vistaMenu=categorias_productos">
                 <img src="./img/menu_nav/tienda.png" alt="Productos">
-                <p>Productos</p>
+                <span>Productos</span>
             </a>
         </li>
         <li>
-            <a href="./?vistaMenu=ofertas">
-                <img src="./img/menu_nav/ofertas.png" alt="Ofertas">
-                <p>Ofertas</p>
-            </a>
+            <a href="./?vistaMenu=ofertas"><img src="./img/menu_nav/ofertas.png" alt="Ofertas"><span>Ofertas</span></a>
         </li>
         <li>
-            <a href="./?vistaMenu=sobre_nosotros">
-                <img src="./img/menu_nav/sobre_nosotros.png" alt="Sobre nosotros">
-                <p>Sobre nosotros</p>
-            </a>
+            <a href="./?vistaMenu=sobre_nosotros"><img src="./img/menu_nav/sobre_nosotros.png" alt="Sobre nosotros"><span>Sobre nosotros</span></a>
         </li>
         <li>
-            <a href="./?vistaMenu=soporte">
-                <img src="./img/menu_nav/soporte.png" alt="Soporte">
-                <p>Soporte</p>
-            </a>
+            <a href="./?vistaMenu=soporte"><img src="./img/menu_nav/soporte.png" alt="Soporte"><span>Soporte</span></a>
         </li>
         <li>
-            <a href="./?vistaMenu=contacto">
-                <img src="./img/menu_nav/contacto.png" alt="Contacto">
-                <p>Contacto</p>
-            </a>
+            <a href="./?vistaMenu=contacto"><img src="./img/menu_nav/contacto.png" alt="Contacto"><span>Contacto</span></a>
         </li>
     </ul>
 </nav>
