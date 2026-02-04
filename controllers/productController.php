a604c7af29da195c6311a3f92760cbb306046650
Arturo Jiménez Aparicio <55811825+Aja1702@users.noreply.github.com>
2026-02-04 13:31:07 +0100

    Primer Commit-2

--- controllers/productController.php
+++ controllers/productController.php
@@ -1,9 +1,9 @@
 <?php
 require_once '../models/Product.php';
 class ProductController {
-public function index($conn) {
-$product = new Product();
-$productos = $product->getAllProducts($conn);
-include '../views/products.php';
-}
+    public function index($conn) {
+        $product = new Product();
+        $productos = $product->getAllProducts($conn);
+        include '../views/products.php';
+    }
 }
