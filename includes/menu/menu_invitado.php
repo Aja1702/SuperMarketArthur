fbf98dffdd4b315a2ec0b1dcf2d7ec9bc1adb3d4
Arturo Jiménez Aparicio <55811825+Aja1702@users.noreply.github.com>
2026-03-13 01:16:23 +0100

    Corregida imagen favoritos.svg->png y copiados assets faltantes

--- includes/menu/menu_invitado.php
+++ includes/menu/menu_invitado.php
@@ -1,43 +1,43 @@
 <nav class="menu-horizontal" role="navigation" aria-label="Menú principal SuperMarketArthur">
     <ul>
         <li>
-            <a href="/SuperMarketArthur/" class="menu-link">
+            <a href="<?php echo BASE_URL; ?>" class="menu-link">
                 <img src="<?php echo BASE_URL; ?>assets/img/menu_nav/home.png" alt="" aria-hidden="true" />
                 <span>Inicio</span>
             </a>
         </li>
         <li>
-            <a href="/SuperMarketArthur/productos" class="menu-link">
+            <a href="<?php echo BASE_URL; ?>productos" class="menu-link">
                 <img src="<?php echo BASE_URL; ?>assets/img/menu_nav/tienda.png" alt="" aria-hidden="true" />
                 <span>Productos</span>
             </a>
         </li>
         <li>
-            <a href="/SuperMarketArthur/favoritos" class="menu-link">
-                <img src="<?php echo BASE_URL; ?>assets/img/menu_nav/favoritos.svg" alt="Favoritos" aria-hidden="true" />
+            <a href="<?php echo BASE_URL; ?>favoritos" class="menu-link">
+                <img src="<?php echo BASE_URL; ?>assets/img/menu_nav/favoritos.png" alt="Favoritos" aria-hidden="true" />
                 <span>Favoritos</span>
             </a>
         </li>
         <li>
-            <a href="/SuperMarketArthur/ofertas" class="menu-link">
+            <a href="<?php echo BASE_URL; ?>ofertas" class="menu-link">
                 <img src="<?php echo BASE_URL; ?>assets/img/menu_nav/ofertas.png" alt="" aria-hidden="true" />
                 <span>Ofertas</span>
             </a>
         </li>
          <li>
-            <a href="/SuperMarketArthur/sobre-nosotros" class="menu-link">
+            <a href="<?php echo BASE_URL; ?>sobre-nosotros" class="menu-link">
                 <img src="<?php echo BASE_URL; ?>assets/img/menu_nav/sobre_nosotros.png" alt="" aria-hidden="true" />
                 <span>Sobre nosotros</span>
             </a>
         </li>
         <li>
-            <a href="/SuperMarketArthur/soporte" class="menu-link">
+            <a href="<?php echo BASE_URL; ?>soporte" class="menu-link">
                 <img src="<?php echo BASE_URL; ?>assets/img/menu_nav/soporte.png" alt="" aria-hidden="true" />
                 <span>Soporte</span>
             </a>
         </li>
         <li>
-            <a href="/SuperMarketArthur/contacto" class="menu-link">
+            <a href="<?php echo BASE_URL; ?>contacto" class="menu-link">
                 <img src="<?php echo BASE_URL; ?>assets/img/menu_nav/contacto.png" alt="" aria-hidden="true" />
                 <span>Contacto</span>
             </a>
